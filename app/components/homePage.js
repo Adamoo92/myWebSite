bd92da01ca24007b0d77e05ce4a52548c1a4bd99
Adamoo92 <daichangle1203@gmail.com>
2024-07-19 14:36:37 +0800

    change home-page

---
+++ app/components/homePage.js
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import catpng from "/public/image/home/cat.png";
+import designHigh from "/public/image/home/design_high.svg";
+import moveNow from "/public/image/home/move_now.svg";
+import {
+  useRive,
+  useStateMachineInput,
+  Layout,
+  Fit,
+  Alignment,
+} from "@rive-app/react-canvas-lite";
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+  const { rive, RiveComponent } = useRive({
+    src: "/image/home/cat.riv",
+    autoplay: true,
+    artboard: "cat",
+    stateMachines: "CatMotion",
+    layout: new Layout({
+      fit: Fit.FitHeight,
+      alignment: Alignment.Center,
+    }),
+  });
+
+  const [screenSize, setScreenSize] = useState({});
+
+  const xAxisInput = useStateMachineInput(rive, "CatMotion", "xAxis");
+  const yAxisInput = useStateMachineInput(rive, "CatMotion", "yAxis");
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) {
+      const bodyRect = body.getBoundingClientRect();
+      setScreenSize({ w: bodyRect.width, h: bodyRect.height });
+    }
+  }, []);
+
+  useEffect(() => {
+    const update = (e) => {
+      if (screenSize && xAxisInput && yAxisInput) {
+        xAxisInput.value = (e.clientX / screenSize.w) * 100;
+        yAxisInput.value = (e.clientY / screenSize.h) * 100;
+      }
+    };
+    window.addEventListener("mousemove", update);
+    return () => {
+      window.addEventListener("mousemove", update);
+    };
+  }, [screenSize, xAxisInput, yAxisInput]);
+
+  return (
+    <section className="w-screen h-screen flex justify-center bg-[#3DB5C3] relative">
+      <Image
+        src={designHigh}
+        alt="design_high_text"
+        className="absolute h-1/4 w-auto top-[20%]"
+      />
+      <Image
+        src={moveNow}
+        alt="move_now_text"
+        className="absolute h-1/7 w-auto top-[50%] ml-[50%]"
+      />
+      <div className="w-screen h-screen relative">
+        <RiveComponent />
+      </div>
+    </section>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { Raleway } from "next/font/google";
 import DetailLine from "./components/detailLine";
 import Contack from "./components/contack";
-import Head from "next/head";
+import HomePage from "./components/homePage";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -13,19 +13,8 @@ const raleway = Raleway({
 export default function Home() {
   return (
     <>
-      <div className="text-ownBlack w-full h-full flex justify-center items-center">
-        <div>
-          <div className="title flex gap-8">
-            <DetailLine />
-            <div
-              className={`${raleway.className} text-8xl italic uppercase select-none`}
-            >
-              <h1>Welcome to</h1>
-              <h1>my website</h1>
-            </div>
-          </div>
-          {/* <Button>Read More</Button> */}
-        </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <HomePage />
       </div>
       <Contack />
     </>
