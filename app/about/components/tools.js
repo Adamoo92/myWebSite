38a8717dfccd0088ddd95a3d8e66bd8fa480749f
Adamoo92 <daichangle1203@gmail.com>
2024-07-24 16:25:52 +0800

    change part of about  tools

--- app/about/components/tools.js
+++ app/about/components/tools.js
@@ -1,247 +1,286 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion, useInView } from "framer-motion";
 import Image from "next/image";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { useRive } from "@rive-app/react-canvas";
+import {
+  iconLists,
+  topBarRightBtn,
+  otherSkillsList,
+  topBarBtnColor,
+} from "../data/tools-icon-data";
 
-const iconLists = [
-  { name: "photoshop", color: "#0EA5E9", rate: 9 },
-  { name: "illustrator", color: "#F97316", rate: 9 },
-  { name: "after effect", color: "#7C3AED", rate: 7.5 },
-  { name: "figma", color: "#E11D48", rate: 9.5 },
-  { name: "framer", color: "#84CC16", rate: 9.2 },
-  { name: "blender", color: "#FBBF24", rate: 7.5 },
-  { name: "affinity", color: "#14B8A6", rate: 8.8 },
-];
-
-const ToolsButton = (props) => {
-  return (
-    <div
-      className="tools-button relative flex justify-center"
-      style={{ width: 100, height: 106 }}
-    >
-      <span
-        className="absolute border-2 rounded-2xl bottom-0"
-        style={{
-          width: 100,
-          height: 90,
-          backgroundColor: "#F3CC7E",
-          borderColor: "#552A19",
-        }}
-      />
-      <span
-        className="absolute border-2 rounded-2xl bottom-2"
-        style={{
-          width: 100,
-          height: 90,
-          backgroundColor: "#FAEEC4",
-          borderColor: "#552A19",
-        }}
-      />
-      <span
-        className="absolute border-2 rounded-xl"
-        style={{
-          width: 80,
-          height: 80,
-          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), ${
-            props.bg || "#0EA5E9"
-          }`,
-          borderColor: "#552A19",
-          top: 10,
-        }}
-      />
-      <motion.div
-        className="absolute border-2 rounded-xl cursor-pointer"
-        style={{
-          width: 80,
-          height: 80,
-          backgroundColor: `${props.bg || "#0EA5E9"}`,
-          borderColor: "#552A19",
-          top: 0,
-        }}
-        whileTap={{ y: 5 }}
-        onClick={props.onClick}
-      >
-        <Image
-          src={`/image/about/tools/icon/${props.src || "photoshop"}.svg`}
-          width={80}
-          height={80}
-          alt={`about-tools-icon-${props.alt}`}
-          className="absolute"
-        />
-        <span
-          className="absolute border-2 bg-white rounded-full"
-          style={{
-            borderColor: "#552A19",
-            width: 20,
-            height: 6,
-            top: 4,
-            right: 13,
-          }}
-        />
-        <span
-          className="absolute border-2 bg-white rounded-full"
-          style={{
-            borderColor: "#552A19",
-            width: 7,
-            height: 6,
-            top: 4,
-            right: 4,
-          }}
-        />
-      </motion.div>
-      {props.show ? (
-        <motion.div
-          className="absolute w-max h-max"
-          layoutId="tools-arrow"
-          style={{ bottom: -40 }}
-        >
-          <Image
-            src="/image/about/tools/arrow.svg"
-            width={28}
-            height={28}
-            alt="about-tools-icon-arrow"
-          />
-        </motion.div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
-};
-
-function Tools() {
+export default function Tools() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: false, amount: 0.4 });
-  const [selected, setSelected] = useState(0);
-
-  const handleClick = (index) => {
-    setSelected(index);
-  };
 
   return (
-    <section
-      className="about-tools-container w-full h-screen pt-28 flex items-center"
-      ref={ref}
-    >
+    <section className="about-tools-container w-full flex flex-col items-center py-24">
+      <ToolsTitle />
+      <TopBar />
       <div
-        className="about-tools-content w-full flex flex-col justify-between items-center snap-center"
-        style={{ height: 600, minHeight: 500 }}
+        className="w-full p-28 pb-40 bg-white flex flex-col gap-8 mt-4 rounded-lg"
+        style={{ boxShadow: "0px 8px 16px 2px #E5CFD0" }}
+        ref={ref}
       >
-        <div
-          className="about-tools-title relative"
-          style={{ width: 272, height: 208, color: "#58261D" }}
-        >
-          <AnimatePresence>
-            {isInView && (
-              <>
+        <AnimatePresence>
+          {isInView && (
+            <>
+              {iconLists.map((item, i) => (
                 <motion.div
-                  key="about-tools-title-1"
-                  className="text-7xl font-bold pt-4 flex justify-center absolute top-0 left-0"
-                  style={{
-                    width: 240,
-                    height: 138,
-                    backgroundImage: "url('/image/about/tools/bubble_1.svg')",
-                    originX: 0,
-                    originY: 1,
-                  }}
-                  initial={{ rotate: -20, x: -200 }}
-                  animate={{ rotate: 0, x: 0 }}
-                  exit={{ rotate: -20, x: -200 }}
-                  duration={{ duration: 0.8, delay: 0.2 }}
+                  className="w-full h-max"
+                  initial={{ x: -60, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: -60, opacity: 0 }}
+                  transition={{ duration: 0.4, delay: 0.4 + 0.1 * i }}
                 >
-                  Tools
+                  <ToolsButton key={i} item={item} />
                 </motion.div>
-                <motion.div
-                  key="about-tools-title-2"
-                  className="font-sans text-xl font-semibold pt-4 flex justify-center absolute right-0 bottom-0 "
-                  style={{
-                    width: 120,
-                    height: 82,
-                    backgroundImage: "url('/image/about/tools/bubble_2.svg')",
-                    originX: 1,
-                    originY: 1,
-                  }}
-                  initial={{ rotate: 20, x: 100 }}
-                  animate={{ rotate: 0, x: 0 }}
-                  exit={{ rotate: 20, x: 100 }}
-                  duration={{ duration: 0.8, delay: 0.2 }}
-                >
-                  常用工具
-                </motion.div>
-              </>
-            )}
-          </AnimatePresence>
-        </div>
-        <div className="about-tools-progress-container flex flex-col gap-3 items-center">
-          <AnimatePresence>
-            {isInView && (
-              <>
-                <motion.div
-                  key="about-tools-progress"
-                  className="about-tools-progress rounded-full flex gap-1 p-2"
-                  style={{
-                    width: 500,
-                    height: 40,
-                    border: "2px solid #58261D",
-                  }}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.4 }}
-                >
-                  <motion.div
-                    className="h-full rounded-full"
-                    animate={{
-                      width: (iconLists[selected].rate / 10) * 484,
-                      backgroundColor: iconLists[selected].color,
-                    }}
-                    transition={{ duration: iconLists[selected].rate / 20 }}
-                  />
-                </motion.div>
-                <motion.div
-                  key="about-tools-progress-text"
-                  className="about-tools-progress-text flex gap-3 text-lg text-ownBlack font-mono capitalize select-none"
-                  initial={{ opacity: 0, y: 16 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 16 }}
-                  transition={{ duration: 0.4, delay: 0.2 }}
-                >
-                  <p>{iconLists[selected].name} /</p>
-                  <motion.div>{iconLists[selected].rate * 10}%</motion.div>
-                </motion.div>
-              </>
-            )}
-          </AnimatePresence>
-        </div>
-        <div className="about-tools-button-group flex gap-8">
-          <AnimatePresence>
-            {isInView && (
-              <>
-                {iconLists.map((item, i) => (
-                  <motion.div
-                    className={`w-max h-max about-tools-button-${i}`}
-                    key={i}
-                    initial={{ scale: 1.2, y: -20, opacity: 0 }}
-                    animate={{ scale: 1, y: 0, opacity: 1 }}
-                    exit={{ scale: 1.2, y: -20, opacity: 0 }}
-                    transition={{ duration: 0.2, delay: 0.1 * i }}
-                  >
-                    <ToolsButton
-                      src={item.name}
-                      bg={item.color}
-                      alt={item.name}
-                      onClick={() => handleClick(i)}
-                      show={selected === i ? true : false}
-                    />
-                  </motion.div>
-                ))}
-              </>
-            )}
-          </AnimatePresence>
-        </div>
+              ))}
+            </>
+          )}
+        </AnimatePresence>
       </div>
+      <OtherSkills />
     </section>
   );
 }
 
-export default Tools;
+// components
+function ToolsTitle() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  const { rive, RiveComponent } = useRive({
+    src: "/image/about/tools/rive/tools.riv",
+    stateMachines: "State Machine",
+    autoplay: true,
+  });
+
+  useEffect(() => {
+    if (isInView && rive) {
+      const interval = setInterval(() => {
+        rive.play();
+      }, 400);
+      interval();
+      return () => clearInterval(interval);
+    }
+  }, [isInView]);
+
+  return (
+    <div className="flex gap-12 w-full h-max justify-center z-10" ref={ref}>
+      <AnimatePresence>
+        <div
+          key="rive-aniamtion-container"
+          className="w-80 h-80 bg-transparent relative cursor-pointer"
+        >
+          {isInView ? (
+            <motion.div
+              key="rive-animation"
+              className="w-full h-full"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={{ duration: 0.4, delay: 0.4 }}
+            >
+              <RiveComponent
+                onPointerEnter={() => rive && rive.pause()}
+                onPointerLeave={() => rive && rive.play()}
+              />
+            </motion.div>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div key="lottie-animation-container" className="w-80 h-80">
+          {isInView && (
+            <motion.div
+              key="lottie-animation"
+              className="w-full h-full"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.4 }}
+            >
+              <Player
+                autoplay
+                loop
+                src="https://lottie.host/a9279429-9a45-42fe-8c1d-b2896fdfb7e6/GBiRtbmp7P.json"
+                style={{ width: 320, height: 320 }}
+                className="flex-none h-max"
+              />
+            </motion.div>
+          )}
+        </div>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+function TopBar() {
+  return (
+    <div className="flex w-full h-[84px] -mt-[84px] relative">
+      <div className="w-24 h-full pb-6 z-10">
+        <motion.div
+          className="w-full h-full flex gap-2 justify-center items-center bg-white rounded-lg cursor-pointer"
+          style={{ boxShadow: "0px 16px 24px 2px #E5CFD0" }}
+          whileHover={{ scale: 1.15, rotate: -10 }}
+        >
+          {topBarBtnColor.map((backgroundColor, i) => (
+            <span
+              key={i}
+              className="w-4 h-4 rounded-full"
+              style={{ backgroundColor }}
+            />
+          ))}
+        </motion.div>
+      </div>
+      <div className="flex-1 h-full pt-4 relative">
+        <div
+          className="w-full h-full bg-white -ml-8 rounded-lg"
+          style={{
+            width: "calc(100% + 64px)",
+            boxShadow: "0px 8px 16px 2px #E5CFD0",
+          }}
+        />
+      </div>
+      <div
+        className="px-2 py-2.5 bg-white rounded-lg flex gap-2 mb-6 z-10"
+        style={{ boxShadow: "0px 16px 24px 2px #E5CFD0" }}
+      >
+        {topBarRightBtn.map((icon, i) => (
+          <motion.div
+            key={i}
+            className="p-1.5 w-max h-max rounded-lg bg-transparent hover:bg-white cursor-pointer"
+            style={{
+              boxShadow: "0px 0px 0px 0px #E5CFD0",
+            }}
+            whileHover={{
+              boxShadow: "0px 16px 24px 2px #E5CFD0",
+              y: -12,
+            }}
+          >
+            {icon}
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const ToolsButton = (props) => {
+  const { item } = props;
+  return (
+    <div className="flex gap-8 w-full">
+      <div className="btn-container flex justify-center pt-2.5 relative">
+        <div className="btn-bg w-[100px] h-[100px] rounded-2xl border-2 border-[#552A19] bg-[#F3CC7E] pb-2.5">
+          <span className="inline-block box-content -ml-0.5 -mt-0.5 w-full h-full rounded-2xl border-2 border-[#552A19] bg-[#FAEEC4]" />
+        </div>
+        <div
+          className="btn-toggle w-20 h-20 bg-[#5EC9FA] border-2 border-[#552A19] rounded-xl absolute top-2"
+          style={{
+            background: `linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), ${
+              item ? item.background : "#5EC9FA"
+            }`,
+          }}
+        >
+          <motion.div
+            className="w-full aspect-square bg-[#5EC9FA] box-content -mt-2.5 -ml-0.5 border-2 border-[#552A19] rounded-xl relative cursor-pointer"
+            style={{
+              backgroundColor: `${item ? item.background : "#5EC9FA"}`,
+            }}
+            whileTap={{ y: 4 }}
+          >
+            <Image
+              src={item ? item.src : "/image/about/tools/icon/photoshop.svg"}
+              width={item ? null : 80}
+              height={item ? null : 80}
+              alt={item ? item.name : "icon"}
+            />
+            <div className="absolute top-2 right-2 flex gap-1 h-1.5">
+              <span className="w-5 h-full rounded-full border-2 border-[#552A19] bg-white"></span>
+              <span className="w-1.5 h-full rounded-full border-2 border-[#552A19] bg-white"></span>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-3 text-ownBlack font-mono capitalize text-lg font-normal">
+        <h2 className="">{item ? item.name : "photoshop"}</h2>
+        <div className="flex gap-4">
+          <div className="flex-1 h-6 p-1 border-2 border-[#58261D] rounded-full">
+            <motion.span
+              className="block w-full h-full rounded-full bg-[#5EC9FA]"
+              style={{
+                backgroundColor: item ? item.background : "#5EC9FA",
+                originX: 0,
+                scaleX: 0,
+              }}
+              animate={{ scaleX: item && item.rate / 10 }}
+            />
+          </div>
+          <span className="w-12">{item ? item.rate * 10 : 90}%</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function OtherSkills() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  return (
+    <div className="w-full h-max -mt-16 flex justify-end" ref={ref}>
+      <AnimatePresence>
+        {isInView ? (
+          <motion.div
+            key="other-skills-animation"
+            className="text-lg font-mono font-normal text-ownBlack w-[440px] bg-white px-10 py-8 flex flex-col gap-6 mr-28"
+            style={{
+              boxShadow: "0px 12px 24px 4px rgba(229, 207, 208, 0.90)",
+              opacity: 0,
+              scale: 1.4,
+              y: 60,
+            }}
+            initial={{ opacity: 0, scale: 1.4, y: 60 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 1.4, y: 60 }}
+            transition={{ duration: 0.4, delay: 0.4, type: "spring" }}
+          >
+            <h1 className="py-2 bg-[#FFEBEC] text-center">Other skills</h1>
+            <>
+              {otherSkillsList.map((item, i) => (
+                <div key={i} className="flex">
+                  <p className="flex-1">{item.name}</p>
+                  <div className="flex gap-2">
+                    <>
+                      {Array(item.rate)
+                        .fill(0)
+                        .map((_black, j) => (
+                          <span
+                            key={j}
+                            className="w-2 h-2 rounded-full bg-ownBlack"
+                          />
+                        ))}
+                      {Array(10 - item.rate)
+                        .fill(0)
+                        .map((_white, k) => (
+                          <span
+                            key={k}
+                            className="w-2 h-2 rounded-full bg-white border border-ownBlack"
+                          />
+                        ))}
+                    </>
+                  </div>
+                </div>
+              ))}
+            </>
+          </motion.div>
+        ) : (
+          <></>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ app/about/data/tools-icon-data.js
@@ -0,0 +1,76 @@
+// skill icon
+import PhotoshopSVG from "../../../public/image/about/tools/icon/photoshop.svg";
+import IllustratorSVG from "../../../public/image/about/tools/icon/illustrator.svg";
+import AfterEffectSVG from "../../../public/image/about/tools/icon/after effect.svg";
+import FigmaSVG from "../../../public/image/about/tools/icon/figma.svg";
+import FramerSVG from "../../../public/image/about/tools/icon/framer.svg";
+import BlenderSVG from "../../../public/image/about/tools/icon/blender.svg";
+import AffinitySVG from "../../../public/image/about/tools/icon/affinity.svg";
+
+export const iconLists = [
+  { name: "photoshop", src: PhotoshopSVG, background: "#5EC9FA", rate: 9 },
+  { name: "illustrator", src: IllustratorSVG, background: "#FF9B61", rate: 9 },
+  {
+    name: "after effect",
+    src: AfterEffectSVG,
+    background: "#A876FF",
+    rate: 7.5,
+  },
+  { name: "figma", src: FigmaSVG, background: "#FF6480", rate: 9.5 },
+  { name: "framer", src: FramerSVG, background: "#A6D263", rate: 9.2 },
+  { name: "blender", src: BlenderSVG, background: "#FBBF24", rate: 7.5 },
+  { name: "affinity", src: AffinitySVG, background: "#8BDCD3", rate: 8.8 },
+];
+
+// left btn
+export const topBarBtnColor = ["#FE5F57", "#FEBC2D", "#2AC840"];
+
+// right btn
+const MinusSvg = () => {
+  return (
+    <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+      <path d="M6 14H22" stroke="#0E0E0F" strokeWidth="3" />
+    </svg>
+  );
+};
+
+const SquareSvg = () => {
+  return (
+    <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+      <rect
+        x="5.5"
+        y="5.5"
+        width="17"
+        height="17"
+        rx="1"
+        stroke="#0E0E0F"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+};
+
+const MultiSvg = () => {
+  return (
+    <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.9983 16.1196L7.12132 22.9965L5 20.8752L11.8769 13.9982L5.00002 7.12132L7.12134 5L13.9983 11.8769L20.8752 5L22.9965 7.12132L16.1196 13.9982L22.9965 20.8752L20.8752 22.9965L13.9983 16.1196Z"
+        fill="#0E0E0F"
+      />
+    </svg>
+  );
+};
+
+export const topBarRightBtn = [<MinusSvg />, <SquareSvg />, <MultiSvg />];
+
+// other skills
+export const otherSkillsList = [
+  { name: "lottie", rate: 6 },
+  { name: "motion", rate: 8 },
+  { name: "use-gesture", rate: 5 },
+  { name: "react-three", rate: 2 },
+  { name: "spline", rate: 4 },
+  { name: "rive", rate: 7 },
+];
